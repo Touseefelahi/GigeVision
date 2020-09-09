da47ded67a0de94f41c573ad291c0f3268b71ecf
Faisal Fagihi <f.fagihi@stira.sa>
2020-09-09 17:24:42 +0300

    Added Window Loading  Event

--- GigeVision.Wpf/Views/DeviceControl.xaml.cs
+++ GigeVision.Wpf/Views/DeviceControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GigeVision.Core.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -18,9 +19,35 @@ namespace GigeVision.Wpf.Views
     /// </summary>
     public partial class DeviceControl : UserControl
     {
+        public CameraRegisterVisibilty CameraRegisterVisibilty
+        {
+            get { return (CameraRegisterVisibilty)GetValue(CameraRegisterVisibiltyProperty); }
+            set { SetValue(CameraRegisterVisibiltyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CameraRegisterVisibilty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CameraRegisterVisibiltyProperty =
+            DependencyProperty.Register("CameraRegisterVisibilty", typeof(CameraRegisterVisibilty), typeof(DeviceControl), new PropertyMetadata(CameraRegisterVisibilty.Beginner));
+
+        public ICommand LoadedWindowCommand
+        {
+            get { return (ICommand)GetValue(LoadedWindowCommandProperty); }
+            set { SetValue(LoadedWindowCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LoadedWindowCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LoadedWindowCommandProperty =
+            DependencyProperty.Register("LoadedWindowCommand", typeof(ICommand), typeof(DeviceControl), new PropertyMetadata(null));
+
         public DeviceControl()
         {
             InitializeComponent();
+            this.Loaded += DeviceControl_Loaded;
+        }
+
+        private void DeviceControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadedWindowCommand?.Execute(null);
         }
     }
 }
