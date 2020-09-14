ece3c9f4f089755c293b6e72bac8c0f5534d6325
Faisal Fagihi <f.fagihi@stira.sa>
2020-09-14 16:30:45 +0300

    Added formula execution test

--- GigeVision.CoreTests/Models/ConverterTests.cs
+++ GigeVision.CoreTests/Models/ConverterTests.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GigeVision.Core.Enums;
+using System.Linq.Expressions;
 
 namespace GigeVision.Core.Models.Tests
 {
@@ -25,6 +26,73 @@ namespace GigeVision.Core.Models.Tests
             //CollectionAssert.AreEqual(new byte[] { 0x00, 0x00, 0x0A, 0x00 }, bytes3);
             var bytes4 = Converter.RegisterStringToByteArray("9978");
             CollectionAssert.AreEqual(new byte[] { 0x00, 0x00, 0x99, 0x78 }, bytes4);
+        }
+
+        [TestMethod()]
+        public void StringFormulaToMathematicalExpression()
+        {
+            var formula = "((varible1/ varible2)/varible1+8)*8";
+            Dictionary<string, uint> registers = new Dictionary<string, uint>();
+            registers.Add("varible1", 5);
+            registers.Add("varible2", 5);
+
+            foreach (var character in formula.ToCharArray())
+            {
+                List<char> opreations = new List<char> { '(', '+', '-', '/', '*', ')' };
+                if (opreations.Where(x => x == character).Count() > 0)
+                {
+                    formula = formula.Replace($"{character}", $" {character} ");
+                }
+            }
+
+            foreach (var word in formula.Split())
+            {
+                foreach (var register in registers)
+                {
+                    if (register.Key.Equals(word))
+                        formula = formula.Replace(word, $"{register.Value}");
+                }
+            }
+
+            var r = Evaluate(formula);
+        }
+
+        private double Evaluate(string expression)
+        {
+            expression = expression.Replace(" ", "");
+            expression = "(" + expression + ")";
+            Stack<string> opreators = new Stack<string>();
+            Stack<double> values = new Stack<double>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                string letter = expression.Substring(i, 1);
+                if (letter.Equals("(")) { }
+                else if (letter.Equals("+")) opreators.Push(letter);
+                else if (letter.Equals("-")) opreators.Push(letter);
+                else if (letter.Equals("*")) opreators.Push(letter);
+                else if (letter.Equals("/")) opreators.Push(letter);
+                else if (letter.Equals("sqrt")) opreators.Push(letter);
+                else if (letter.Equals(")"))
+                {
+                    int count = opreators.Count;
+                    while (count > 0)
+                    {
+                        string opreator = opreators.Pop();
+                        double value = values.Pop();
+                        if (opreator.Equals("+")) value = values.Pop() + value;
+                        else if (opreator.Equals("-")) value = values.Pop() - value;
+                        else if (opreator.Equals("*")) value = values.Pop() * value;
+                        else if (opreator.Equals("/")) value = values.Pop() / value;
+                        else if (opreator.Equals("sqrt")) value = Math.Sqrt(value);
+                        values.Push(value);
+
+                        count--;
+                    }
+                }
+                else values.Push(double.Parse(letter));
+            }
+            return values.Pop();
         }
     }
 }
