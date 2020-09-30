dbdf667cd4453f1dbcd08dc2b8a9e3d141f992e5
Faisal Fagihi <f.fagihi@stira.sa>
2020-09-30 11:18:05 +0300

    improved WriteValue method

--- GigeVision.Wpf/DTO/CameraRegisterDTO.cs
+++ GigeVision.Wpf/DTO/CameraRegisterDTO.cs
@@ -5,41 +5,79 @@ using Prism.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 
 namespace GigeVision.Wpf.DTO
 {
     public class CameraRegisterDTO
     {
         public ICamera Camera { get; }
-        public CameraRegister CameraRegister { get; set; }
-        public DelegateCommand SetValueCommand { get; set; }
+        public CameraRegisterContainer CameraRegisterContainer { get; set; }
+        public DelegateCommand<object> SetValueCommand { get; set; }
 
-        public CameraRegisterDTO(ICamera camera, CameraRegister cameraRegister)
+        public CameraRegisterDTO(ICamera camera, CameraRegisterContainer cameraRegisterContainer)
         {
             Camera = camera;
-            CameraRegister = cameraRegister;
-            SetValueCommand = new DelegateCommand(WriteValue);
+            CameraRegisterContainer = cameraRegisterContainer;
+            SetValueCommand = new DelegateCommand<object>(WriteValue);
         }
 
-        private async void WriteValue()
+        private async void WriteValue(object newValue)
         {
-            GvcpReply gvcpReply;
-            if (CameraRegister.Type == CameraRegisterType.Integer)
+            GvcpReply gvcpReply = null;
+
+            if (CameraRegisterContainer.Register.AccessMode == CameraRegisterAccessMode.RO)
+                return;
+
+            if (newValue != CameraRegisterContainer.Register.Value)
             {
                 await Camera.Gvcp.TakeControl(false);
-                if (CameraRegister.Value is KeyValuePair<string, uint> dictionaryValue)
-                    gvcpReply = (await Camera.Gvcp.WriteRegisterAsync(CameraRegister.Address, dictionaryValue.Value));
-                else if (CameraRegister.Value is string stringValue)
-                    gvcpReply = (await Camera.Gvcp.WriteRegisterAsync(CameraRegister.Address, uint.Parse(stringValue)));
-                else if (CameraRegister.Value is uint uintValue)
-                    gvcpReply = (await Camera.Gvcp.WriteRegisterAsync(CameraRegister.Address, uintValue));
-            }
+                switch (CameraRegisterContainer.Type)
+                {
+                    case CameraRegisterType.Integer:
+                        var integerRegister = CameraRegisterContainer.TypeValue as IntegerRegister;
+                        try
+                        {
+                            gvcpReply = (await Camera.Gvcp.WriteRegisterAsync(integerRegister.Register.Address, uint.Parse((string)newValue)));
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Error {ex}");
+                        }
+                        break;
 
-            if (CameraRegister.Type == CameraRegisterType.String)
-            {
-                await Camera.Gvcp.TakeControl(true);
+                    case CameraRegisterType.Float:
+                        break;
 
-                gvcpReply = (await Camera.Gvcp.WriteMemoryAsync(CameraRegister.Address, BitConverter.ToUInt32(Encoding.ASCII.GetBytes((string)CameraRegister.Value), 0)));
+                    case CameraRegisterType.StringReg:
+                        var stringRegister = CameraRegisterContainer.TypeValue as CameraRegister;
+                        //gvcpReply = (await Camera.Gvcp.WriteRegisterAsync(stringRegister.Address, uint.Parse((string)stringRegister.Value)));
+                        gvcpReply = (await Camera.Gvcp.WriteMemoryAsync(stringRegister.Address, BitConverter.ToUInt32(Encoding.ASCII.GetBytes((string)stringRegister.Value), 0)));
+                        break;
+
+                    case CameraRegisterType.Enumeration:
+                        var enumeration = CameraRegisterContainer.TypeValue as Enumeration;
+                        gvcpReply = (await Camera.Gvcp.WriteRegisterAsync(enumeration.Register.Address, ((KeyValuePair<string, uint>)enumeration.Register.Value).Value));
+
+                        break;
+
+                    case CameraRegisterType.Command:
+                        var command = CameraRegisterContainer.TypeValue as CommandRegister;
+                        gvcpReply = (await Camera.Gvcp.WriteRegisterAsync(command.Register.Address, (uint)command.Register.Value));
+                        break;
+
+                    case CameraRegisterType.Boolean:
+                        var boolean = CameraRegisterContainer.TypeValue as BooleanRegister;
+                        gvcpReply = (await Camera.Gvcp.WriteRegisterAsync(boolean.Register.Address, (uint)newValue));
+                        break;
+
+                    default:
+                        break;
+                }
+                if (gvcpReply.Status != GvcpStatus.GEV_STATUS_SUCCESS)
+                {
+                    MessageBox.Show($"{gvcpReply.Status}");
+                }
             }
         }
     }
