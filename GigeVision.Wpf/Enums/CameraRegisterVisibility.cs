94058817e68681d980f57f2082faaa11b012f907
Faisal Fagihi <f.fagihi@stira.sa>
2020-09-30 11:18:21 +0300

    added new file

---
+++ GigeVision.Wpf/Enums/CameraRegisterVisibility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GigeVision.Wpf.Enums
+{
+    /// <summary>
+    /// Camera Registrer Visibility
+    /// </summary>
+    public enum CameraRegisterVisibility
+    {
+        /// <summary>
+        /// Beginner (First Level)
+        /// </summary>
+        Beginner,
+
+        /// <summary>
+        /// Expert(Second Level)
+        /// </summary>
+        Expert,
+
+        /// <summary>
+        /// Guru (Third Level)
+        /// </summary>
+        Guru
+    }
+}
