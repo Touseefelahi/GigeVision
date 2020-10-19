c7b049eb6b8e664c97ff72591a1e4f07b856a1ec
Faisal Fagihi <f.fagihi@stira.sa>
2020-10-19 19:35:03 +0300

    Add Device Control Test Project

---
+++ DeviceControl.Test.Wpf/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using DeviceControl.Test.Wpf.Views;
+using System.Windows;
+
+namespace DeviceControl.Test.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
