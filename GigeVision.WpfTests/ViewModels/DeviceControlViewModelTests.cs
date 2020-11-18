f9c2c11f46c5d9040874a5732df3a517086ce2d0
Touseef Elahi <touseefelahi@ymail.com>
2020-11-18 12:28:49 +0300

    Bump Prism version

--- GigeVision.WpfTests/ViewModels/DeviceControlViewModelTests.cs
+++ GigeVision.WpfTests/ViewModels/DeviceControlViewModelTests.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DeviceControl.Wpf.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DeviceControl.Wpf.ViewModels;
 using GigeVision.Core.Interfaces;
 using GigeVision.Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 
 namespace GigeVision.Wpf.ViewModels.Tests
@@ -12,14 +9,14 @@ namespace GigeVision.Wpf.ViewModels.Tests
     [TestClass()]
     public class DeviceControlViewModelTests
     {
-        private IGvcp Gvcp { get; set; }
-        private DeviceControlViewModel DeviceControl { get; set; }
-
         public DeviceControlViewModelTests()
         {
             Gvcp = new Gvcp("192.168.10.244");
             DeviceControl = new DeviceControlViewModel(Gvcp);
         }
+
+        private IGvcp Gvcp { get; set; }
+        private DeviceControlViewModel DeviceControl { get; set; }
 
         [TestMethod()]
         public async Task DeviceControlViewModelTest()
