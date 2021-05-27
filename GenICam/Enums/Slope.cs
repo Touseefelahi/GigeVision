91ba9f059b8d3ba21f7985db5655836f518cb744
Faisal Fagihi <f.fagihi@stira.sa>
2021-05-27 14:26:34 +0300

    Add New Enum

---
+++ GenICam/Enums/Slope.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenICam
+{
+    public enum Slope
+    {
+        None,
+        Increasing,
+        Decreasing,
+        Varying,
+        Automatic
+    }
+}
