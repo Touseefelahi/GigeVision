7b0820bda6c2cb55b8ad19ade7e9b9c80ddce3d2
Faisal Fagihi <f.fagihi@stira.sa>
2021-05-27 14:32:36 +0300

    Add Dictionary For Temproray Registers Values

---
+++ GenICam/Models/TempDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenICam
+{
+    public static class TempDictionary
+    {
+        public static Dictionary<string, object> Formula { get; set; }
+
+        static TempDictionary()
+        {
+            Formula = new Dictionary<string, object>();
+        }
+
+        public static void Clear()
+        {
+            Formula.Clear();
+        }
+    }
+}
