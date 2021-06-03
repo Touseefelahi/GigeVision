6c12705a982e349f27b518f439207229043338d5
Touseef Elahi <touseefelahi@ymail.com>
2021-06-03 08:31:50 +0300

    Add FPS Indicator in WPF test project

--- GigeVisionWpfTest2/MainWindow.xaml.cs
+++ GigeVisionWpfTest2/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -25,36 +26,59 @@ namespace GigeVisionWpfTest2
         private Camera camera;
 
         private Gvcp gvcp;
+        private System.Timers.Timer timer;
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
             camera = new();
-            camera.IP = "192.168.10.197";
+            camera.IP = "192.168.10.170";
             gvcp = new();
             camera.FrameReady += FrameReady;
             image.WidthImage = 640;
             image.HeightImage = 480;
+            timer = new();
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = 1000;
         }
 
         public byte[] RawBytes { get; set; }
 
         public int FrameCounter { get; set; }
 
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            timer.Stop();
+            timer.Start();
+            fps.Dispatcher.Invoke(() =>
+            {
+                fps.Text = $"FPS: {FrameCounter}";
+            });
+            FrameCounter = 0;
+        }
+
         private void FrameReady(object sender, byte[] e)
         {
             image.Dispatcher.Invoke(() => image.RawBytes = e);
+            FrameCounter++;
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            timer.Start();
             await camera.StartStreamAsync("192.168.10.172").ConfigureAwait(false);
         }
 
         private async void Button_Click_1(object sender, RoutedEventArgs e)
         {
             await camera.StopStream().ConfigureAwait(false);
+            timer.Stop();
+            FrameCounter = 0;
+            fps.Dispatcher.Invoke(() =>
+            {
+                fps.Text = $"FPS: {FrameCounter}";
+            });
         }
     }
 }
