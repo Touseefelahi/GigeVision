7894d29df16eadc72e8edfd0dd093c8aca8c22a1
Touseef Elahi <touseefelahi@ymail.com>
2021-06-10 15:59:04 +0300

    Document

--- GigeVision.Core/Models/GvspInfo.cs
+++ GigeVision.Core/Models/GvspInfo.cs
@@ -1,14 +1,43 @@
 ﻿namespace GigeVision.Core.Models
 {
+    /// <summary>
+    /// GVSP info
+    /// </summary>
     public class GvspInfo
     {
-        public int PayloadOffset;
-        public int PacketIDIndex;
-        public int DataIdentifier;
-        public int DataEndIdentifier;
-        public int PacketLength;
-        public int PayloadSize;
-        public bool IsValid;
-        public int FinalPacketID;
+        /// <summary>
+        /// Payload offset for GVSP Header length
+        /// </summary>
+        public int PayloadOffset { get; set; }
+
+        /// <summary>
+        /// Packet ID index in the GVSP header
+        /// </summary>
+        public int PacketIDIndex { get; set; }
+
+        /// <summary>
+        /// Data identifier 0x02 for old version and 0x82 for new
+        /// </summary>
+        public int DataIdentifier { get; set; }
+
+        /// <summary>
+        /// Data end identifier 0x04 for old version and 0x84 for new
+        /// </summary>
+        public int DataEndIdentifier { get; set; }
+
+        /// <summary>
+        /// Normal Packet Length, Payload with Header
+        /// </summary>
+        public int PacketLength { get; set; }
+
+        /// <summary>
+        /// Payload size only, without header
+        /// </summary>
+        public int PayloadSize { get; set; }
+
+        /// <summary>
+        /// Final Packet ID
+        /// </summary>
+        public int FinalPacketID { get; set; }
     }
 }
