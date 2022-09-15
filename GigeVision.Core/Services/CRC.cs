a3a13d06cead5004c4a5cfd3cc1fd8ab3665af51
Touseef Elahi <touseefelahi@ymail.com>
2022-09-15 15:07:16 +0300

    Add Allow App through firewall with admin rights

---
+++ GigeVision.Core/Services/CRC.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace Crc
+{
+    public enum InitialCrcValue
+    {
+        Zeros,
+        NonZero1 = 0xffff,
+        NonZero2 = 0x1D0F
+    }
+
+    /// <summary>
+    /// Simple Crc function I found <see href="http://www.sanity-free.com/133/crc_16_ccitt_in_csharp.html">here</see>
+    /// </summary>
+    public class Crc16Ccitt
+    {
+        private const ushort poly = 4129;
+        private ushort[] table = new ushort[256];
+        private ushort initialValue = 0;
+
+        public ushort ComputeChecksum(string inputString)
+        {
+            return ComputeChecksum(Encoding.ASCII.GetBytes(inputString));
+        }
+
+        public ushort ComputeChecksum(byte[] bytes)
+        {
+            ushort crc = this.initialValue;
+            for (int i = 0; i < bytes.Length; ++i)
+            {
+                crc = (ushort)((crc << 8) ^ table[((crc >> 8) ^ (0xff & bytes[i]))]);
+            }
+            return crc;
+        }
+
+        public byte[] ComputeChecksumBytes(byte[] bytes)
+        {
+            ushort crc = ComputeChecksum(bytes);
+            return BitConverter.GetBytes(crc);
+        }
+
+        public Crc16Ccitt(InitialCrcValue initialValue = InitialCrcValue.Zeros)
+        {
+            this.initialValue = (ushort)initialValue;
+            ushort temp, a;
+            for (int i = 0; i < table.Length; ++i)
+            {
+                temp = 0;
+                a = (ushort)(i << 8);
+                for (int j = 0; j < 8; ++j)
+                {
+                    if (((temp ^ a) & 0x8000) != 0)
+                    {
+                        temp = (ushort)((temp << 1) ^ poly);
+                    }
+                    else
+                    {
+                        temp <<= 1;
+                    }
+                    a <<= 1;
+                }
+                table[i] = temp;
+            }
+        }
+    }
+}
--- GigeVision.Core/Services/NetworkService.cs
+++ GigeVision.Core/Services/NetworkService.cs
@@ -1,4 +1,8 @@
-﻿using System.Net;
+﻿using Crc;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 
@@ -51,7 +55,7 @@ namespace GigeVision.Core
                     }
 
                     //I know this logic is stupid, but its simple
-                    if (preferStaticIP) 
+                    if (preferStaticIP)
                     {
                         if (address.PrefixOrigin != PrefixOrigin.Dhcp)
                         {
@@ -83,6 +87,59 @@ namespace GigeVision.Core
             return mostSuitableIp != null
                 ? mostSuitableIp.Address.ToString()
                 : "";
+        }
+
+        /// <summary>
+        /// This method requires admin rights
+        /// </summary>
+        /// <returns></returns>
+        public static void AllowAppThroughFirewall()
+        {
+            string appFullPath = Process.GetCurrentProcess().MainModule.FileName;
+            string appName = Path.GetFileNameWithoutExtension(appFullPath);
+
+            //Generating unique ID for this application
+            Crc16Ccitt checksum = new();
+            var crc = checksum.ComputeChecksum(appFullPath);
+            string ruleName = $"Gvsp-{appName}{crc:X4}".Replace(" ", "");
+            //Checking if we already have the access
+            var command = $"/C netsh advfirewall firewall show rule name = all | findstr  /r /s /i /m /c:\"\\<{ruleName}\\>\"";
+            var reply = RunCommand(command);
+
+            if (string.IsNullOrEmpty(reply))
+            {
+                command = $"/C netsh advfirewall firewall add rule name =\"{ruleName}\" dir=in action=allow program=\"{appFullPath}\" enable=yes";
+                RunCommandAdmin(command);
+            }
+        }
+
+        private static string RunCommand(string command)
+        {
+            Process p = new();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = Environment.ExpandEnvironmentVariables("%SystemRoot%") + @"\System32\cmd.exe";
+            p.StartInfo.Arguments = command;
+            p.Start();
+            // Read the output stream first and then wait.
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            return output;
+        }
+
+        private static string RunCommandAdmin(string command)
+        {
+            Process p = new();
+            p.StartInfo.UseShellExecute = true;
+            //p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = Environment.ExpandEnvironmentVariables("%SystemRoot%") + @"\System32\cmd.exe";
+            p.StartInfo.Arguments = command;
+            p.StartInfo.Verb = "runas";
+            p.Start();
+            // Read the output stream first and then wait.
+            // string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            return null;
         }
     }
 }
