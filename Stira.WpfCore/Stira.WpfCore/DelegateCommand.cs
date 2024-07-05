07ecc84d246642ea8c5db813be4cd1fb2d7504f0
Jakob Heinrich <jakob.heinrich@tigris.de>
2024-07-05 16:13:28 +0200

    add submodule

---
+++ Stira.WpfCore/Stira.WpfCore/DelegateCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Stira.WpfCore
+{
+    public class DelegateCommand : ICommand
+    {
+        #region Private Members
+
+        /// <summary>
+        /// The action to run
+        /// </summary>
+        private readonly Action mAction;
+
+        #endregion Private Members
+
+        #region Public Events
+
+        /// <summary>
+        /// The event thats fired when the <see cref="CanExecute(object)"/> value has changed
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        #endregion Public Events
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DelegateCommand(Action action)
+        {
+            mAction = action;
+        }
+
+        #endregion Constructor
+
+        #region Command Methods
+
+        /// <summary>
+        /// A relay command can always execute
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the commands Action
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            mAction();
+        }
+
+        #endregion Command Methods
+    }
+}
